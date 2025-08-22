09e016260c954352f760f15877af7dd4b245c7bd
adishgarg <gargadi456@gmail.com>
2025-08-22 10:21:36 +0530

    push

--- src/services/api/endpoint.js
+++ src/services/api/endpoint.js
@@ -2,21 +2,21 @@ export const ENDPOINTS = {
     EMPLOYEE_CREATE: '/employees/create',
     EMPLOYEE_BY_ID: (id) => `/employees/get/${id}`,
     EMPLOYEES_ALL: '/employees/getAll',
-    EMPLOYEES_DELETE: (id) => `employees/delete/${id}`,
-    EMPLOYEES_UPDATE: (id) => `employees/update/${id}`,
-    EMPLOYEES_UPDATEPASS: 'employees/updatePass',
+    EMPLOYEES_DELETE: (id) => `/employees/delete/${id}`,
+    EMPLOYEES_UPDATE: (id) => `/employees/update/${id}`,
+    EMPLOYEES_UPDATEPASS: '/employees/updatePass',
     
     AUTH_LOGIN: '/auth/login',
 
-    TASK_ALL: 'tasks/getALl',
-    TASK_CREATE: 'tasks/create',
-    TASK_BY_ID: (id) => `tasks/getOne/${id}`,
-    TASK_UPDATE:(id) => `tasks/update/${id}`,
-    TASK_DELETE: (id) => `tasks/delete/${id}`,
+    TASK_ALL: '/tasks/getALl',
+    TASK_CREATE: '/tasks/create',
+    TASK_BY_ID: (id) => `/tasks/getOne/${id}`,
+    TASK_UPDATE:(id) => `/tasks/update/${id}`,
+    TASK_DELETE: (id) => `/tasks/delete/${id}`,
 
 
 
-    ORGANIZATION_ALL: 'organizations/getAll',
+    ORGANIZATION_ALL: '/organizations/getAll',
 
 
 }
