159fe51377696663e01196414db5b27964bf14f6
Akshat Anand <108303349+AKsHaT075@users.noreply.github.com>
2025-09-03 02:06:34 +0530

    Under Work

---
+++ src/Page/AllEvents.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const AllEvents = () => {
+  return (
+    <div>
+      <h1>All Events</h1>
+      <ul>
+        <li>Event 1</li>
+        <li>Event 2</li>
+        <li>Event 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default AllEvents
+
